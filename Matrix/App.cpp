cc74082580c4078e9e85d1621889f24f12b8f208
Homescu Monica Daniella <monihomescu@yahoo.com>
2022-11-30 16:30:51 +0200

    Update App.cpp

--- Matrix/App.cpp
+++ Matrix/App.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include "Matrix.h"
-#include "ExtendedTest.h"
 #include "ShortTest.h"
 using namespace std;
 
 int main() {
 	testAll();
-	testAllExtended();
-	cout << "Test End" << endl;
-	system("pause");
+	return 0;
 }
