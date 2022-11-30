321d7c31fcf9dfbc8e33e192c97ffd85e61b0880
Homescu Monica Daniella <monihomescu@yahoo.com>
2022-11-30 16:29:05 +0200

    Update App.cpp

--- PriorityQueue/App.cpp
+++ PriorityQueue/App.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include "PriorityQueue.h"
-#include "ExtendedTest.h"
 #include "ShortTest.h"
 using namespace std;
 
 int main() {
 	testAll();
-	testAllExtended();
-	cout << "End" << endl;
-	system("pause");
+	return 0;
 }
