cd34b2e87147bd496b1ad29239f461c08f37c374
Homescu Monica Daniella <monihomescu@yahoo.com>
2022-11-30 16:26:07 +0200

    Update App.cpp

--- SortedIteratedList/App.cpp
+++ SortedIteratedList/App.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include "ShortTest.h"
-#include "ExtendedTest.h"
 
 int main() {
     	testAll();
-    	testAllExtended();
-    	std::cout<<"Finished IL Tests!"<<std::endl;
-	system("pause");
+	return 0;
 }
