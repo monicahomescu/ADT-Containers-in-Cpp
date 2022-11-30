1205cb2e29bf0fe2bd2ba9bb46d8e1b292b0c88d
Homescu Monica Daniella <monihomescu@yahoo.com>
2022-11-30 12:42:18 +0200

    Update ShortTest.cpp

--- SortedMap/ShortTest.cpp
+++ SortedMap/ShortTest.cpp
@@ -13,23 +13,23 @@ bool relatie1(TKey cheie1, TKey cheie2) {
 }
 
 void testAll() {
-    SortedMap sm(relatie1);
-    assert(sm.size() == 0);
-    assert(sm.isEmpty());
-    sm.add(1,2);
-    assert(sm.size() == 1);
-    assert(!sm.isEmpty());
-    assert(sm.search(1)!=NULL_TVALUE);
-    TValue v =sm.add(1,3);
-    assert(v == 2);
-    assert(sm.search(1) == 3);
-    SMIterator it = sm.iterator();
-    it.first();
-    while (it.valid()){
-    	TElem e = it.getCurrent();
-    	assert(e.second != NULL_TVALUE);
-    	it.next();
-    }
-    assert(sm.remove(1) == 3);
-    assert(sm.isEmpty());
+    	SortedMap sm(relatie1);
+    	assert(sm.size() == 0);
+    	assert(sm.isEmpty());
+    	sm.add(1,2);
+    	assert(sm.size() == 1);
+    	assert(!sm.isEmpty());
+    	assert(sm.search(1)!=NULL_TVALUE);
+    	TValue v =sm.add(1,3);
+    	assert(v == 2);
+    	assert(sm.search(1) == 3);
+    	SMIterator it = sm.iterator();
+    	it.first();
+    	while (it.valid()) {
+    		TElem e = it.getCurrent();
+    		assert(e.second != NULL_TVALUE);
+    		it.next();
+    	}
+    	assert(sm.remove(1) == 3);
+    	assert(sm.isEmpty());
 }
